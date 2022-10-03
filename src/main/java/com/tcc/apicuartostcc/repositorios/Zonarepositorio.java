455c3fba8cbb3564b8bd0246a97190dc7aee3e63
206 <206@academicos.cesde.edu.co>
2022-10-03 12:12:01 -0500

    03/10/2022

---
+++ src/main/java/com/tcc/apicuartostcc/repositorios/Zonarepositorio.java
@@ -0,0 +1,7 @@
+package com.tcc.apicuartostcc.repositorios;
+
+import com.tcc.apicuartostcc.entidades.Zona;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Zonarepositorio extends JpaRepository<Zona,Integer> {
+}
